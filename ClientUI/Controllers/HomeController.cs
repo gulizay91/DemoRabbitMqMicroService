f3ca3aeab74620ebc711bf64226227b3e2625b61
gulizay91 <gulizay91@gmail.com>
2020-01-10 08:16:27 +0300

    init

---
+++ ClientUI/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Contract;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClientUI.Controllers
+{
+  public class HomeController : Controller
+  {
+    
+    private readonly IRequestClient<Command, Event> _requestClient;
+
+    public HomeController(IRequestClient<Command, Event> requestClient)
+    {
+      _requestClient = requestClient;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    //[HttpPut("{id}")]
+    public async Task<IActionResult> Submit()//(string id, CancellationToken cancellationToken)
+    {
+      try
+      {
+        //Event result = await _requestClient.Request(new { SendMessage = id.ToString() }, cancellationToken);
+        Event result = await _requestClient.Request(new { SendMessage = "123" });
+        return Accepted(result.ReceiveMessage);
+      }
+      catch (RequestTimeoutException exception)
+      {
+        return StatusCode((int)HttpStatusCode.RequestTimeout);
+      }
+    }
+
+  }
+}
---
+++ Contract/Command.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Contract
+{
+  public class Command
+  {
+    public string SendMessage { get; set; }
+  }
+}
